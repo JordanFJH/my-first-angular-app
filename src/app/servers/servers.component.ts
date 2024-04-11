c8b131782f9db57f17b98a40c0344b03b9803f79
Jordan <higgsfj@gmail.com>
2024-04-11 11:37:50 -0400

    added button to create own server and starter logic for it

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: "./servers.component.html",
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "";
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000)
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!"
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value
+  }
 }
